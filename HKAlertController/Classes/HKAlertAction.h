2d5af8c44db4839324e977dd3a8136c7f5b34f71
haikun.xiu <haikun.xiu@shopee.com>
2021-01-22 17:54:52 +0800

    refactor(action): implement action class

--- HKAlertController/Classes/HKAlertAction.h
+++ HKAlertController/Classes/HKAlertAction.h
@@ -9,7 +9,33 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class HKAlertAction;
+
+/// alert action click handler
+typedef void(^HKAlertActionHandler)(HKAlertAction *action);
+
+/// alert action style
+typedef NS_ENUM(NSUInteger, HKAlertActionStyle) {
+    HKAlertActionStyleDefault       = 0,
+    HKAlertActionStyleCancel        = 1,
+    HKAlertActionStyleDestructive   = 2,
+};
+
+
 @interface HKAlertAction : NSObject
+
+/// action initilization
+/// @param title action button title
+/// @param style action button style
+/// @param handler action click handler
++ (instancetype)actionWithTitle:(nullable NSString *)title
+                          style:(HKAlertActionStyle)style
+                        handler:(nullable HKAlertActionHandler)handler;
+
+@property (nonatomic, copy, nullable, readonly) NSString *title;
+
+@property (nonatomic, assign, readonly) HKAlertActionStyle style;
+@property (nonatomic, assign, getter=isEnabled) BOOL enabled;
 
 @end
 
--- HKAlertController/Classes/HKAlertAction.m
+++ HKAlertController/Classes/HKAlertAction.m
@@ -7,6 +7,26 @@
 
 #import "HKAlertAction.h"
 
+@interface HKAlertAction ()
+
+@property (nonatomic, copy, nullable) NSString *title;
+@property (nonatomic, copy, nullable) HKAlertActionHandler handler;
+
+@property (nonatomic, assign) HKAlertActionStyle style;
+
+@end
+
 @implementation HKAlertAction
+
++ (instancetype)actionWithTitle:(nullable NSString *)title
+                          style:(HKAlertActionStyle)style
+                        handler:(nullable HKAlertActionHandler)handler {
+    HKAlertAction *alertAction = [[HKAlertAction alloc] init];
+    alertAction.title = title;
+    alertAction.style = style;
+    alertAction.handler = handler;
+    alertAction.enabled = YES;
+    return alertAction;
+}
 
 @end
